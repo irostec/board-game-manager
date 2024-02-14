f8007a80d5a62a2e525747f8fae1e8071f2e5fee
Luis Manuel de la Cruz Ortiz <irostecinc@gmail.com>
2024-02-14 19:49:44 -0400

    initialization: Add basic Spring Boot configuration
    - Update .gitignore to exclude autogenerated files.
    - Add Spring Boot Actuator dependency to verify that the web service is booting and running correctly.
    - Configure Jacoco plugin to assess code coverage.

---
+++ src/main/java/com/irostec/boardgamemanager/BoardGameManagerApplication.java
@@ -0,0 +1,13 @@
+package com.irostec.boardgamemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irostec/boardgamemanager/BoardgamemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irostec.boardgamemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
